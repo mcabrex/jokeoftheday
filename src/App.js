309571984940ab5ac5014d3a5274c631ab1f3a1a
AnthonyClausing <AnthonyClausing@gmail.com>
2021-05-13 19:05:45 -0400

    api call for profile pic

--- src/App.js
+++ src/App.js
@@ -1,45 +1,65 @@
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  // const FB = window.FB
+export default class App extends Component {
+    // const FB = window.FB
   // console.log(window.FB,'HELLOO inside app component')
-  function loginWithFB() {
+  constructor(props) {
+    super(props);
+    this.state = {userId: '', imageUrl: 'https://c8.alamy.com/comp/M4MCPC/funny-clown-with-a-gun-pistol-isolated-on-white-background-M4MCPC.jpg'};
+
+    this.loginWithFB = this.loginWithFB.bind(this)
+    this.setStateHack = this.setStateHack.bind(this) 
+  }
+
+  setStateHack(key,value){
+    this.setState({[key]: value})
+  }
+  loginWithFB() {
     try{    
-      window.FB && window.FB.login(function(response) {
-    console.log('response loginWithFB',response)
-        if (response.authResponse) {
-          console.log('Welcome!  Fetching your information.... ');
-          window.FB.api('/me', function(response) {
-            console.log('Good to see you, ' + response.name + '.');
-          });
+      window.FB && window.FB.login(function({authResponse}) {
+        if (authResponse) {
+          // console.log('Welcome!  Fetching your information.... ');
+           window.FB.api('/me', function(response) {
+            window.FB.api(
+              `/${response.id}/picture?redirect=false`,
+              function (response) {
+                if (response && !response.error) {
+                  // console.log('response from LOGIN BUTTON PICTURE CALL',response)
+                  console.log(this,'this deepest', response.data.url)  
+                  this.setState({imageUrl: response.data.url})
+                }
+                console.log(response.error)
+            }.bind(this))
+          }.bind(this))
         } else {
           console.log('User cancelled login or did not fully authorize.');
         }
-      })
+      }.bind(this))
     } catch(e) {
       console.log(e,'error in loginWithFB')
     }
   }
 
-  return (
-    <div className="App">
-      <div id="joke-container">
-        <h1 id="name">JOKE GENERATOR</h1>
-        <img id="clown-img" alt="clown" src="https://c8.alamy.com/comp/M4MCPC/funny-clown-with-a-gun-pistol-isolated-on-white-background-M4MCPC.jpg" />
-        <div id="description">
-          Jokes on you!
+  render  () {
+    return (
+      <div className="App">
+        <div id="joke-container">
+          <h1 id="name">JOKE GENERATOR</h1>
+          <img id="clown-img" alt="clown" src={this.state.imageUrl} />
+          <div id="description">
+            Jokes on you!
+          </div>
+          <button id="fb-auth" className="btn" onClick={this.loginWithFB}>
+            Sign in to Facebook
+          </button>
+          {/* <div className="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="false"></div> */}
+          {/* fake loading spinner */}
+          {/* text doc for generator*/}
+          {/*ouath to facebook  */}
         </div>
-        <button id="fb-auth" className="btn" onClick={loginWithFB}>
-          Sign in to Facebook
-        </button>
-        {/* <div className="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="false"></div> */}
-        {/* fake loading spinner */}
-        {/* text doc for generator*/}
-        {/*ouath to facebook  */}
+
       </div>
-
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
--- src/_helpers/init-facebook-sdk.js
+++ src/_helpers/init-facebook-sdk.js
@@ -12,41 +12,6 @@ export function initFacebookSdk() {
               xfbml: true,
               version: 'v10.0'
           });
-          // console.log(window.FB,'inside initFacebook')
-          // auto authenticate with the api if already logged in with facebook
-   
-          // let fbAuthButton = document.getElementById('fb-auth')
-          // fbAuthButton.addEventListener('click', function(evt){
-          window.FB.api(
-              "/105682011703154/picture?redirect=false",
-              function (response) {
-                if (response && !response.error) {
-                  console.log('response in init-facebook fb.api',response)
-                }
-                console.log(response.error)
-          })
-          // );
-          window.FB.getLoginStatus(function(response) {
-            console.log(response, 'in init-facebook fb.getLoginStatus')
-            if (response.status === 'connected') {
-              // The user is logged in and has authenticated your
-              // app, and response.authResponse supplies
-              // the user's ID, a valid access token, a signed
-              // request, and the time the access token 
-              // and signed request each expire.
-              var uid = response.authResponse.userID;
-              var accessToken = response.authResponse.accessToken;
-              console.log(uid, accessToken,'userid and accessToken')
-            } else if (response.status === 'not_authorized') {
-              // The user hasn't authorized your application.  They
-              // must click the Login button, or you must call FB.login
-              // in response to a user gesture, to launch a login dialog.
-            } else {
-              // The user isn't logged in to Facebook. You can launch a
-              // login dialog with a user gesture, but the user may have
-              // to log in to Facebook before authorizing your application.
-            }
-           });
       };
       // load facebook sdk script
       (function (d, s, id) {
