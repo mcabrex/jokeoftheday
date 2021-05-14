1fdd7928dea60abbf9a41f30b38b548cb5e58dcf
mcabrex <mrcabardo@gmail.com>
2021-05-14 02:48:05 -0400

    Implemented react-facebook-login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import FacebookLogin from 'react-facebook-login';
+
 
 function App() {
+  const [imageSrc, setImageSrc] = useState("https://c8.alamy.com/comp/M4MCPC/funny-clown-with-a-gun-pistol-isolated-on-white-background-M4MCPC.jpg");
+
+  const apikey = process.env.REACT_APP_FACEBOOK_APP_ID
+  
+  const responseFacebook = (response) => {
+    console.log(response);
+    let {picture} = response
+    setImageSrc(picture.data.url)
+  }
+  
   return (
     <div className="App">
       <div id="joke-container">
         <h1 id="name">JOKE GENERATOR</h1>
-        <img id="clown-img" src="https://c8.alamy.com/comp/M4MCPC/funny-clown-with-a-gun-pistol-isolated-on-white-background-M4MCPC.jpg" />
+        <img id="clown-img" src={imageSrc} />
         <div id="description">
           Jokes on you!
         </div>
-        <button class="btn oauth">
+        <FacebookLogin
+          appId={apikey}
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={responseFacebook} 
+        />
+        {/* <button class="btn oauth">
           Sign in to Facebook
-        </button>
+        </button> */}
         {/* fake loading spinner */}
         {/* text doc for generator*/}
         {/*ouath to facebook  */}
