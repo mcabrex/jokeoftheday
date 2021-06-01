5d8115086d18a4ce6df9435197d3026fb16598be
mcabrex <mrcabardo@gmail.com>
2021-06-01 12:33:32 -0400

    Updated color variable names

--- src/sections/GetStarted/index.js
+++ src/sections/GetStarted/index.js
@@ -14,7 +14,7 @@ const GetStarted = (params) => {
         <div className="start" id="started">
             <h1>Whatever Just Give Me The Goods</h1>
             <p>Yea alright we've said enough, if you wanna see the perfect joke in action all you have to do is click the button down below to get started. Best of all, it's completely free!</p>
-            <TextButton label="Get Started!"/>
+            <TextButton label="Get Started!" hover="true" onClick={()=>{console.log('clicked')}}/>
         </div>
     )
 }
