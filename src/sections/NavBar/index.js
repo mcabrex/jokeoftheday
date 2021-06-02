fe69a86b6f6898fde64cbf8084b191aa5278c907
mcabrex <mrcabardo@gmail.com>
2021-06-01 22:08:50 -0400

    Update navbar

--- src/sections/NavBar/index.js
+++ src/sections/NavBar/index.js
@@ -3,9 +3,12 @@ import './NavBar.css'
 const NavBar = (params) => {
     return (
         <nav>
-            <a className="nav-about" href="#about">About Us</a>
-            <a className="nav-work" href="#work">The Work</a>
-            <a className="nav-started" href="#started">Get Started</a>
+            <ul className="main-nav">
+                <li><a className="nav-home">Home</a></li>
+                <li className="push"><a className="nav-about" href="#about">About Us</a></li>
+                <li><a className="nav-work" href="#work">The Work</a></li>
+                <li><a className="nav-started" href="#started">Get Started</a></li>
+            </ul>
         </nav>
     )
 }
